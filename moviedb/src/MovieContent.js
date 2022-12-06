85560677e84f4e12098c3884c66500f2f44175cf
Jimmieost <jimmie.ost@utb.ecutbildning.se>
2022-12-06 19:37:36 +0100

    Added components, added onclick event for modal.

--- moviedb/src/MovieContent.js
+++ moviedb/src/MovieContent.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { Modal, show, Button } from "react-bootstrap";
+import React, { useState } from "react";
 const API_IMG = "https://image.tmdb.org/t/p/w500/";
 
 //Lägger ut filmernas innehåll på sidan
@@ -9,11 +10,44 @@ const MovieContent = ({
   release_date,
   overview,
 }) => {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
   return (
-    <div>
-      <h1>{title}</h1>
-      <img src={API_IMG + poster_path} alt="Movie Poster"></img>
-      <p>{overview}</p>
+    <div className="movie-card card text-center bg-dark mb-3">
+      <div className="card-body">
+        <img
+          className="card-img-top"
+          style={{ width: "14rem" }}
+          src={API_IMG + poster_path}
+        />
+        <div className="card-body">
+          <button type="button" className="btn-btn-dark" onClick={handleShow}>
+            View More
+          </button>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title></Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img className="card-img-top" src={API_IMG + poster_path} />
+              <h3>{title}</h3>
+              <h4>Vote: {vote_average}</h4>
+              <h5>Realease Date{release_date}</h5>
+              <br></br>
+              <h6>Overview</h6>
+              <p>{overview}</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      </div>
     </div>
   );
 };
